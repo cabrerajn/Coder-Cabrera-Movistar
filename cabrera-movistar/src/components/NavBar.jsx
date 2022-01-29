233dbb51bde3ac5c5babcd89478f136b5e69fa09
cabrerajn <92774517+cabrerajn@users.noreply.github.com>
2022-01-29 01:23:26 -0300

    Desafío 3

---
+++ cabrera-movistar/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <>
+            <ul>
+                <li><button><img src="https://tienda.movistar.com.ar/media/logo/stores/1/logo-movistar.png" /></button></li>
+                <li><button>Alcatel</button></li>
+                <li><button>Huawei</button></li>
+                <li><button>LG</button></li>
+                <li><button>Motorola</button></li>
+                <li><button>Nokia</button></li>
+                <li><button>Samsung</button></li>
+                <li><button>Sony</button></li>
+                <li><button>TCL</button></li>
+            </ul>
+        
+        </>
+    )
+}
