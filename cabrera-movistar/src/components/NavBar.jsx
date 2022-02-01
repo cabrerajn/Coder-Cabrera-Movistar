ee5bfc6c3ad11c4a794a3151dec45db9265847a8
cabrerajn <92774517+cabrerajn@users.noreply.github.com>
2022-02-01 18:05:27 -0300

    Parcial

--- cabrera-movistar/src/components/NavBar.jsx
+++ cabrera-movistar/src/components/NavBar.jsx
@@ -8,14 +8,9 @@ export default function NavBar() {
             <div id="navbar">
                 <ul>
                     <li><button className="boton-menu"><Link to={"/"}><img src="https://tienda.movistar.com.ar/media/logo/stores/1/logo-movistar.png" /></Link></button></li>
-                    <li><button className="boton-menu">Alcatel</button></li>
-                    <li><button className="boton-menu">Huawei</button></li>
                     <li><button className="boton-menu"><Link to={"/category/LG"}>LG</Link></button></li>
                     <li><button className="boton-menu"><Link to={"/category/Motorola"}>Motorola</Link></button></li>
-                    <li><button className="boton-menu">Nokia</button></li>
                     <li><button className="boton-menu"><Link to={"/category/Samsung"}>Samsung</Link></button></li>
-                    <li><button className="boton-menu">Sony</button></li>
-                    <li><button className="boton-menu">TCL</button></li>
                     <CartWidget />
                 </ul>
             </div>
