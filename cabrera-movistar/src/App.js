b373fa270c381336bbdd9542dbffef9564a82397
cabrerajn <92774517+cabrerajn@users.noreply.github.com>
2022-02-01 11:46:16 -0300

    Intermedio

--- cabrera-movistar/src/App.js
+++ cabrera-movistar/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import NavBar from './components/NavBar';
+import CategoryContainer from './components/CategoryContainer';
 
 
 export default function App() {
@@ -13,17 +14,22 @@ export default function App() {
       <BrowserRouter>
         <div><NavBar /></div>
         <Switch>
-          {/* ruta home */}
+          
           <Route exact path="/">
             <div className="mainBanner">
                 <img src="https://tienda.movistar.com.ar/media/mageplaza/bannerslider/banner/image/s/u/summersale_tienda-min.jpg" />
             </div> 
             <ItemListContainer />
           </Route>
-
+          
           <Route path="/item/:itemId">
             <ItemDetailContainer />
           </Route>
+
+          <Route path="/category/:categoriaId">
+            <CategoryContainer />
+          </Route>
+
 
         </Switch>
 
--- cabrera-movistar/src/components/NavBar.jsx
+++ cabrera-movistar/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import CartWidget from "./CartWidget";
 
 export default function NavBar() {
@@ -6,13 +7,13 @@ export default function NavBar() {
         <>
             <div id="navbar">
                 <ul>
-                    <li><button className="boton-menu"><img src="https://tienda.movistar.com.ar/media/logo/stores/1/logo-movistar.png" /></button></li>
+                    <li><button className="boton-menu"><Link to={"/"}><img src="https://tienda.movistar.com.ar/media/logo/stores/1/logo-movistar.png" /></Link></button></li>
                     <li><button className="boton-menu">Alcatel</button></li>
                     <li><button className="boton-menu">Huawei</button></li>
-                    <li><button className="boton-menu">LG</button></li>
-                    <li><button className="boton-menu">Motorola</button></li>
+                    <li><button className="boton-menu"><Link to={"/category/LG"}>LG</Link></button></li>
+                    <li><button className="boton-menu"><Link to={"/category/Motorola"}>Motorola</Link></button></li>
                     <li><button className="boton-menu">Nokia</button></li>
-                    <li><button className="boton-menu">Samsung</button></li>
+                    <li><button className="boton-menu"><Link to={"/category/Samsung"}>Samsung</Link></button></li>
                     <li><button className="boton-menu">Sony</button></li>
                     <li><button className="boton-menu">TCL</button></li>
                     <CartWidget />
